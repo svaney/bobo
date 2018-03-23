5b442b90de41afe77cc3a4aa9ee96e238a9fe829
giorgi <gmarg10@freeuni.edu.ge>
2018-03-23 17:13:41 +0400

    - some style changes

--- app/src/main/java/com/bobo/gmargiani/bobo/ui/activites/MainActivity.java
+++ app/src/main/java/com/bobo/gmargiani/bobo/ui/activites/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bobo.gmargiani.bobo.ui.activites;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,6 +9,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.bobo.gmargiani.bobo.R;
 
